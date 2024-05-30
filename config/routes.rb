fbdcccee395851b80270106dc821c37677fe3109
Olivia Statman <olivia.statman@kellogg.northwestern.edu>
2024-05-30 23:01:44 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+
+  # Login/logout
+  get("/login", { :controller => "sessions", :action => "new" })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
 end
