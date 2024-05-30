b2b5f12f25738795b9a48134923682dee6d67b5e
Olivia Statman <olivia.statman@kellogg.northwestern.edu>
2024-05-30 23:44:37 +0000

    more login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by({ "username" => params["username"] })
+    @user = User.find_by({ "email" => params["email"] })
     if @user != nil
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
         flash["notice"] = "You are now logged-in."
-        redirect_to "/"
+        redirect_to "/places"
       else
         flash["notice"] = "Sorry, invalid username or password."
         redirect_to "/login"
